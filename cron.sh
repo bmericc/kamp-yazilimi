63518509049da5d45d85a5d83016a98f0c9fca22
Bahri Meriç Canlı <bahri@bahri.info>
2024-01-19 17:05:42 +0300

    cron

---
+++ cron.sh
@@ -0,0 +1,2 @@
+export MUDUR_CONFIG=/var/www/kamp-yazilimi/kampyazilim.conf
+/var/www/kamp-yazilimi/venv/bin/python /var/www/kamp-yazilimi/mudur/mudur/send_scheduled_email.py "/var/www/kamp-yazilimi/mudur/" "not_approved_trainess_after_approval_period_ends"
